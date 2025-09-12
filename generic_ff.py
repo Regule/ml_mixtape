0c5c7c2212aa9b502605022f7ad4ee94d6c1428f
Regule <regule@runbox.com>
2025-09-12 15:52:04 +0200

    Ran auto pep8

--- generic_ff.py
+++ generic_ff.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
-#coding: utf-8
+# coding: utf-8
 '''
 This scripts trains a simple feed forward neural network based on given train data CSV and
 then tests it with data from test CSV. You have to give a comma separated list of input
 columns as well as outut columns. Type of task (regression or classification) as well as 
 the architecture of network are inferred from data.
 '''
-#===================================================================================================
+# ==================================================================================================
 #                                             IMPORTS
-#===================================================================================================
+# ==================================================================================================
 # Future imports
 from __future__ import annotations
 
@@ -20,34 +20,38 @@ from dataclasses import dataclass
 from typing import Literal, Tuple, Optional, Sequence
 
 
-#===================================================================================================
-#                                      LITERALS AND CONSTS 
-#===================================================================================================
+# ==================================================================================================
+#                                      LITERALS AND CONSTS
+# ==================================================================================================
 REGRESSION_ID: str = 'regression'
 BINARY_CLASSIFIER_ID: str = 'binary_classification'
 MULTI_CLASSIFIER_ID: str = 'multiclass_classification'
 TaskType = Literal[REGRESSION_ID, BINARY_CLASSIFIER_ID, MULTI_CLASSIFIER_ID]
 
-#===================================================================================================
-#                                          LOGGING 
-#===================================================================================================
+# ==================================================================================================
+#                                          LOGGING
+# ==================================================================================================
+
+
 class VerbosityLevel(Enum):
     ''' Verbosity levels compatiblile with Keras'''
     SILENT = 0,
     INFO = 1,
-    DEBUG =2
+    DEBUG = 2
 
-    def to_python_logging_verbosity(self)-> int:
+    def to_python_logging_verbosity(self) -> int:
         return {
-                VerbosityLevel.SILENT : 60, # Value higher than logging.CRITICAL
-                VerbosityLevel.INFO : logging.INFO,
-                VerbosityLevel.DEBUG : logging.DEBUG
-                }[self]
+            VerbosityLevel.SILENT: 60,  # Value higher than logging.CRITICAL
+            VerbosityLevel.INFO: logging.INFO,
+            VerbosityLevel.DEBUG: logging.DEBUG
+        }[self]
 
-#===================================================================================================
-#                             CONFIGURATION AND ARGUMENT PARSING 
-#===================================================================================================
-def arg_column_list(txt: str)-> Tuple[str,...]:
+# ==================================================================================================
+#                             CONFIGURATION AND ARGUMENT PARSING
+# ==================================================================================================
+
+
+def arg_column_list(txt: str) -> Tuple[str, ...]:
     '''
     Function that converts comma separated list of strings into a
     tuple of strings. This is intended to be used by argparse 
@@ -55,10 +59,12 @@ def arg_column_list(txt: str)-> Tuple[str,...]:
     '''
     column_names = [name.strip() for name in txt.split(',') if name.strip()]
     if not column_names:
-        raise argparse.ArgumentTypeError('Expected a nonepty, comma separated, list.')
+        raise argparse.ArgumentTypeError(
+            'Expected a nonepty, comma separated, list.')
     return tuple(column_names)
 
-def arg_validation_split(txt: str)-> float:
+
+def arg_validation_split(txt: str) -> float:
     try:
         val = float(txt)
         if not (0.0 < val < 0.5):
@@ -69,58 +75,68 @@ def arg_validation_split(txt: str)-> float:
         raise argparse.ArgumentTypeError('Validation split must be a float, value '
                                          f'{txt} given instead.')
 
-def arg_positive_int(txt: str)-> int:
+
+def arg_positive_int(txt: str) -> int:
     try:
         val = int(txt)
         if not val > 0:
-            raise argparse.ArgumentTypeError('Argument value must be greater than 0')
+            raise argparse.ArgumentTypeError(
+                'Argument value must be greater than 0')
         return val
     except ValueError:
-        raise argparse.ArgumentTypeError(f'Unable to convert "{txt}" to valid integer')
+        raise argparse.ArgumentTypeError(
+            f'Unable to convert "{txt}" to valid integer')
 
-def arg_int(txt: str)-> int:
+
+def arg_int(txt: str) -> int:
     try:
         val = int(txt)
         return val
     except ValueError:
-        raise argparse.ArgumentTypeError(f'Unable to convert "{txt}" to valid integer')
+        raise argparse.ArgumentTypeError(
+            f'Unable to convert "{txt}" to valid integer')
 
-def arg_verbosity_level(txt: str)-> VerbosityLevel:
+
+def arg_verbosity_level(txt: str) -> VerbosityLevel:
     try:
         return VerbosityLevel[txt.upper()]
     except KeyError:
         raise argparse.ArgumentTypeError(f'Verbosity level {txt} not recognized available '
                                          f'values are {[e._name_ for e in VerbosityLevel]}')
 
-#---------------------------------------------------------------------------------------------------
-#                                Config class 
+# ---------------------------------------------------------------------------------------------------
+#                                Config class
+
+
 @dataclass(frozen=True)
 class Config:
-    train_csv: str # Path to CSV with training data (inputs and outputs)
-    test_csv: str # Path to CSV with test data (inputs and outputs)
-    model_out: str # Path to file in which model will be saved
-    history_out: str|None # Path to CSV where learning history will be stored,
-                          # if set to None history will not be saved.
-    input_cols: Tuple[str,...] # Columns that will be used as input,
-                              # must exist in train and test CSV
-    output_cols: Tuple[str,...] # Columns that will be used as output,
-                               # must exist in train and test CSV
-    validation_split: float # Part of data that will be used for validation,
-                            # must be between 0.0 and 0.5
-    random_seed: int # A seed used for all random generators, if set to 0 time will be used
-    epochs: int # Maximum number of epochs for which network will be trained.
-    verbosity: VerbosityLevel # Program verbosity
+    train_csv: str  # Path to CSV with training data (inputs and outputs)
+    test_csv: str  # Path to CSV with test data (inputs and outputs)
+    model_out: str  # Path to file in which model will be saved
+    history_out: str | None  # Path to CSV where learning history will be stored,
+    # if set to None history will not be saved.
+    input_cols: Tuple[str, ...]  # Columns that will be used as input,
+    # must exist in train and test CSV
+    output_cols: Tuple[str, ...]  # Columns that will be used as output,
+    # must exist in train and test CSV
+    validation_split: float  # Part of data that will be used for validation,
+    # must be between 0.0 and 0.5
+    random_seed: int  # A seed used for all random generators, if set to 0 time will be used
+    epochs: int  # Maximum number of epochs for which network will be trained.
+    verbosity: VerbosityLevel  # Program verbosity
 
     @staticmethod
-    def from_args(argv: Optional[Sequence[str]] = None)-> Config:
+    def from_args(argv: Optional[Sequence[str]] = None) -> Config:
         prsr = argparse.ArgumentParser(
-                description=__doc__,
-                formatter_class=argparse.RawTextHelpFormatter,
-                )
-        
+            description=__doc__,
+            formatter_class=argparse.RawTextHelpFormatter,
+        )
+
         # Declare arguments
-        prsr.add_argument('--train_csv', required=True, type=str, help='Path to training CSV')
-        prsr.add_argument('--test_csv', required=True, type=str, help='Path to test CSV')
+        prsr.add_argument('--train_csv', required=True,
+                          type=str, help='Path to training CSV')
+        prsr.add_argument('--test_csv', required=True,
+                          type=str, help='Path to test CSV')
         prsr.add_argument('--input_cols', required=True, type=arg_column_list,
                           help='Comma separated names of input feature columns.',
                           )
@@ -135,12 +151,12 @@ class Config:
                           help='Random seed for reproducibility, if not give timer is used as seed')
         prsr.add_argument('--epochs', type=arg_positive_int, default=100,
                           help='Maximum number of epochs with early stopping (default: 100)')
-        prsr.add_argument('--history_out', type=str, default=None, 
+        prsr.add_argument('--history_out', type=str, default=None,
                           help='File to which learning history will be saved'
                           ', if this argument is not set history will not be saved at all.')
         prsr.add_argument('--verbosity', type=arg_verbosity_level,
-                          default = VerbosityLevel.INFO,
-                          help = 'Verbosity level, available value are '
+                          default=VerbosityLevel.INFO,
+                          help='Verbosity level, available value are '
                           f'{[e._name_ for e in VerbosityLevel]}'
                           ' (default: INFO)')
 
@@ -149,27 +165,28 @@ class Config:
 
         # Returning config
         return Config(
-                train_csv = args.train_csv,
-                test_csv = args.test_csv,
-                input_cols = args.input_cols,
-                output_cols = args.output_cols,
-                validation_split = args.validation_split,
-                random_seed = args.random_seed,
-                epochs = args.epochs,
-                verbosity = args.verbosity,
-                model_out = args.model_out,
-                history_out = args.history_out
-                )
+            train_csv=args.train_csv,
+            test_csv=args.test_csv,
+            input_cols=args.input_cols,
+            output_cols=args.output_cols,
+            validation_split=args.validation_split,
+            random_seed=args.random_seed,
+            epochs=args.epochs,
+            verbosity=args.verbosity,
+            model_out=args.model_out,
+            history_out=args.history_out
+        )
 
-#===================================================================================================
-#                                         MAIN 
-#===================================================================================================
+# ==================================================================================================
+#                                         MAIN
+# ==================================================================================================
+
+
 def main():
     cfg: Config = Config.from_args()
     print(cfg)
     print(cfg.verbosity.to_python_logging_verbosity())
 
+
 if __name__ == '__main__':
     main()
-
-
