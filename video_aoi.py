7377bbac89a4a557c9bcd486e26baaa8d2343569
Regule <regule@runbox.com>
2025-09-18 13:03:38 +0200

    Tested concept

--- video_aoi.py
+++ video_aoi.py
@@ -21,7 +21,7 @@ import pathlib
 import time
 import sys
 from dataclasses import dataclass
-from typing import Optional, Sequence
+from typing import Optional, Sequence, Tuple
 
 # Library specific imports
 import numpy as np
@@ -57,11 +57,25 @@ def arg_fps(txt: str) -> int:
             f'Unable to convert "{txt}" to valid integer')
 
 
+def arg_scale(txt: str) -> float:
+    try:
+        val: float = float(txt)
+        if 0.0 < val < 1.0:
+            return val
+        raise argparse.ArgumentTypeError(
+            f'Scale argument must be in range between 0 and 1, value {val} given instead.')
+    except ValueError:
+        raise argparse.ArgumentTypeError(
+            f'Unable to convert "{txt}" to valid float.')
+
+
 @dataclass(frozen=True)
 class Config:
 
     video_path: pathlib.Path  # Path to video that will be processed
     fps_limit: int  # Maximum number of frames per second in playback, if set to zero no limit applied
+    # Scale of AOI map related to source image (must be between 0.0 an 1.0)
+    map_scale: float
 
     @staticmethod
     def from_args(argv: Optional[Sequence[str]] = None) -> Config:
@@ -75,6 +89,8 @@ class Config:
                           type=arg_file_path, help='Path to video that will be processed')
         prsr.add_argument('--fps_limit', default=0,
                           type=arg_fps, help='Maximum number of frames per second in playback')
+        prsr.add_argument('--map_scale', default=1.0,
+                          type=arg_scale, help='Scale of AOI map related to source image (must be between 0.0 an 1.0)')
 
         # Parsing arguments
         args: argparse.Namespace = prsr.parse_args(argv)
@@ -82,7 +98,8 @@ class Config:
         # Returning config
         return Config(
             video_path=args.video_path,
-            fps_limit=args.fps_limit
+            fps_limit=args.fps_limit,
+            map_scale=args.map_scale
         )
 
 # ==================================================================================================
@@ -93,6 +110,35 @@ class Config:
 def panic(msg: str) -> None:
     print(f'Critical - {msg}')
     sys.exit()
+
+
+@dataclass
+class ImageSize:
+
+    x: int  # Horizontal size (width) of image in pixels
+    y: int  # Vertical size (height) of image in pixels
+
+    def to_cv_tuple(self) -> Tuple[int, int]:
+        return (self.y, self.x)
+    
+    def to_np_tuple(self) -> Tuple[int, int]:
+        return (self.x, self.y)
+
+    @staticmethod
+    def from_tuple(src: Tuple[int, ...]) -> ImageSize:
+        if len(src) < 2:
+            raise ValueError(
+                f'Attempting to generate image size for one dimensional vector.')
+        return ImageSize(src[0], src[1])
+
+    def can_encapsulate(self, other: ImageSize) -> bool:
+        return self.x >= other.x and self.y >= other.y
+
+    def is_same_ratio(self, other: ImageSize) -> bool:
+        return self.x//other.x == self.y//other.y
+
+    def scale(self, scale: float) -> ImageSize:
+        return ImageSize(x=int(self.x*scale), y=int(self.y*scale))
 
 # ==================================================================================================
 #                                         VIDEO FRAME
@@ -110,6 +156,9 @@ class VideoFrame:
         copied_data.setflags(write=False)
         return VideoFrame(copied_data, self.timestamp, self.nr)
 
+    def get_size(self) -> ImageSize:
+        return ImageSize.from_tuple(self.data.shape)
+
     @staticmethod
     def from_ndarray(frame_array: npt.NDArray[np.uint8], nr: Optional[int] = None):
         data = frame_array.copy()
@@ -122,27 +171,71 @@ class VideoFrame:
 
 
 class FrameProcessor:
-    ''' Instances of this class are used for processing frames. The correct usage is:
-
-        processor.prepare(frame)
-        processed_frame = processor.process(frame)
-
-        First step adjusts dynamic parameters of processor based on difference between frames
-        and second one applies changes to frame.
-        IMPORTANT - processors do not change frame given as an argument but return updated version.
-    '''
-
-    # Last frame data. It can be None when processing first frame or reset was forced
-    last_frame_: npt.NDArray[np.uint8] | None = None
-
-    def prepare(self, frame: VideoFrame) -> None:
-        ''' If processor have any dynamic properties that must be adjusted for new frame
-         this is a place to set them. For most simple processors this method does nothing '''
-        pass
 
     def process(self, frame: VideoFrame) -> VideoFrame:
         ''' Dummy processor simply returns a copy of source frame'''
         return frame.copy()
+
+
+class AOIdetector(FrameProcessor):
+
+    # Last frame data. It can be None when processing first frame or reset was forced
+    last_frame_: npt.NDArray[np.uint8] | None = None
+    map_size_: ImageSize  # Target size of map and downscaled frame
+    # Number of frames after which we again consider whole image AOI, if set to zero attention mechanism is disabled
+    attention_reset_period_: int
+    # Number of frames remining before whole image will be analyzed
+    attention_remining_: int
+    diff_threshold_: int # Difference in brightnes for which we consider pixel a AOI
+
+    def __init__(self, map_size: ImageSize, diff_threshold:int, attention_reset_period: int = 0) -> None:
+        super().__init__()
+        self.map_size_ = map_size
+        self.attention_reset_period_ = attention_reset_period
+        self.attention_remining_ = attention_reset_period
+        self.diff_threshold_ = diff_threshold
+
+    def process(self, frame: VideoFrame) -> VideoFrame:
+        # Scaling recieved frame to required map size, if image would have
+        # to be upscaled or have different ratio we throw ValueError
+        source_size: ImageSize = frame.get_size()
+        if not source_size.can_encapsulate(self.map_size_):
+            raise ValueError(
+                f'AOImapper - map size {self.map_size_} cannot be encapsulate by image with size of {source_size}')
+        if not self.map_size_.is_same_ratio(source_size):
+            raise ValueError(
+                f'AOImapper - map size {self.map_size_} is of different ratio than source {source_size}')
+        resized_frame = cv.resize(frame.data, self.map_size_.to_cv_tuple())
+
+        # If this is first frame we simply return whole frame as AOI
+        if self.last_frame_ is None:
+            self.last_frame_ = resized_frame # type: ignore
+            return VideoFrame(data=np.asarray(np.ones(self.map_size_.to_np_tuple())*255, dtype=np.uint8),
+                              timestamp=frame.timestamp,
+                              nr=frame.nr)
+
+        # If attention mechanism is active and we reached the limit of attention frames we return
+        # whole frame as AOI
+        if self.attention_reset_period_ > 0 and self.attention_remining_ <= 0:
+            self.attention_remining_ = self.attention_reset_period_
+            self.last_frame_ = resized_frame # type: ignore
+            return VideoFrame(data=np.asarray(np.ones(self.map_size_.to_np_tuple())*255, dtype=np.uint8),
+                              timestamp=frame.timestamp,
+                              nr=frame.nr)
+
+        # If neither of pervious special cases occured we decrement remining attention,
+        # if attention mechanism is used, and calculate AOI. Pixels for which brightness value 
+        # changed over given threshold will be considered AOI
+        if self.attention_reset_period_ > 0:
+            self.attention_remining_-= 1
+        mask = np.abs(self.last_frame_ - resized_frame) > self.diff_threshold_
+        aoi_map = np.where(mask, 255, 0).astype(np.uint8)
+        self.last_frame_ = resized_frame # type: ignore
+        return VideoFrame(data=np.asarray(aoi_map, dtype=np.uint8),
+                          timestamp=frame.timestamp,
+                          nr=frame.nr)
+
+        
 
 # ==================================================================================================
 #                                    VIDEO SOURCE AND SINK
@@ -210,9 +303,6 @@ class SimpleDisplay:
     def __init__(self, window_name: str) -> None:
         self.window_name_ = window_name
 
-    def __del__(self):
-        cv.destroyWindow(self.window_name_)
-
     def spin(self):
         cv.imshow(self.window_name_, self.frame_.data)
 
@@ -220,8 +310,8 @@ class SimpleDisplay:
         if cv.waitKey(1) & 0xFF == ord('q'):
             raise KeyboardInterrupt('User pressed "q" to close GUI.')
 
-    def set_frame(self, frame: VideoFrame)->None:
-            self.frame_ = frame
+    def set_frame(self, frame: VideoFrame) -> None:
+        self.frame_ = frame
 
 # ==================================================================================================
 #                                           MAIN
@@ -231,21 +321,22 @@ class SimpleDisplay:
 def main(argv: Optional[Sequence[str]] = None) -> None:
     cfg: Config = Config.from_args()
 
+    source: VideoSource = VideoSource(
+        str(cfg.video_path.resolve()), cfg.fps_limit)
+    display: SimpleDisplay = SimpleDisplay('Video (press "q" to quit)')
+    aoi_detector: AOIdetector = AOIdetector(
+        source.frame.get_size().scale(cfg.map_scale), 255)
+
     try:
-        source: VideoSource = VideoSource(str(cfg.video_path.resolve()), cfg.fps_limit)
-        display: SimpleDisplay = SimpleDisplay('Video (press "q" to quit)')
+        while True:
+            source.spin()
+            aoi_map: VideoFrame = aoi_detector.process(source.frame)
+            display.set_frame(aoi_map)
+            display.spin()
 
-        try:
-            while True:
-                source.spin()
-                display.set_frame(source.frame)
-                display.spin()
+    except (EOFError, KeyboardInterrupt) as e:
+        print(f'Display stopped, reason : {e}')
 
-        except (EOFError, KeyboardInterrupt) as e:
-            print(f'Display stopped, reason : {e}')
-    except Exception as e:
-        # An exception happend that we are unable to handle
-        panic(str(e))
 
 
 if __name__ == '__main__':
